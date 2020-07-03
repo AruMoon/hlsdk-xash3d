c2a74f97a1b312beddf8c96d327fbb442add4e92
nillerusr <nillerusr@gmail.com>
2020-07-03 21:11:51 +0300

    add upload to vk

---
+++ scripts/upload-to-vk.py
@@ -0,0 +1,26 @@
+import requests,os,sys,json,random
+from datetime import datetime
+
+now = datetime.now()
+dt = now.strftime("%d/%m/%Y %H:%M:%S")
+
+token = os.getenv('VK_TOKEN')
+peer = 2000000091
+attachs=''
+
+def vk(method,**kwargs):
+	param = {'v':'5.91','access_token':token}
+	param.update(kwargs)
+	ret = requests.post('https://api.vk.com/method/'+method, data=param)
+	return ret.json()
+
+srv=vk('docs.getMessagesUploadServer', peer_id=peer)
+
+for i in sys.argv[1:]:
+	os.rename(i, i+i[-1])
+	with open(i+i[-1], 'r') as f:
+		br = requests.post(srv['response']['upload_url'],files={'file': f}).json()
+		doc=vk('docs.save',file=br['file'],title=i)['response']['doc']
+		attachs+='doc'+str(doc['owner_id'])+'_'+str(doc['id'])+','
+
+vk('messages.send', peer_id=peer, message='Build finished at '+dt, attachment=attachs,random_id=random.randint(0,2**10))
