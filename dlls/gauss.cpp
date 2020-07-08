a987fafe2086795e4097a331b4cef07295802da1
nillerusr <nillerusr@gmail.com>
2020-07-08 07:03:55 +0000

    Revert "add mp_allow_gaussjump"
    This reverts commit e84ec67fce4bdf2767887d99b087d0f38fa81cbd.

--- dlls/gauss.cpp
+++ dlls/gauss.cpp
@@ -25,7 +25,6 @@
 #include "shake.h"
 #include "gamerules.h"
 #include "game.h"
-#include "gravgunmod.h"
 
 #define	GAUSS_PRIMARY_CHARGE_VOLUME	256// how loud gauss is while charging
 #define GAUSS_PRIMARY_FIRE_VOLUME	450// how loud gauss is when discharged
@@ -354,7 +353,7 @@ void CGauss::StartFire( void )
 			m_pPlayer->pev->velocity = m_pPlayer->pev->velocity - gpGlobals->v_forward * flDamage * 5;
 		}
 
-		if( !g_pGameRules->IsMultiplayer() || !mp_allow_gaussjump.value )
+		if( !g_pGameRules->IsMultiplayer() )
 		{
 			// in deathmatch, gauss can pop you up into the air. Not in single play.
 			m_pPlayer->pev->velocity.z = flZVel;
